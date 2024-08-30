6df30ac89a2c75f1e7a6199cd40af3d8afae2469
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-08-30 05:24:07 +0300

    chore: add routes for school, class, course,subject

---
+++ src/app/api/timetable/course/route.ts
@@ -0,0 +1,24 @@
+import { courseService } from "@/contollers/CourseService";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const data = await courseService.getAll();
+    return NextResponse.json({ data, sucess: true });
+  } catch (error: any) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const data = await courseService.create(body);
+    return NextResponse.json({ data, sucess: true }, {status:201});
+  } catch (error: any) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
+}
+
+
+
