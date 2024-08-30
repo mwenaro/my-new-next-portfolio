294543edb7ef3ff838e913f48134c9583fa83e79
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-08-30 04:45:10 +0300

    chore : add emails usinf react-email-components

---
+++ emails/confirm.tsx
@@ -0,0 +1,2 @@
+import {default as ConfirmationEmail} from "../src/app/_components/emails/confirmation"
+export default ConfirmationEmail
---
+++ emails/hello.tsx
@@ -0,0 +1,32 @@
+import { Tailwind, Button, Html, Head, Body } from "@react-email/components";
+
+const Email = () => {
+  return (
+    <Tailwind
+      config={{
+        theme: {
+          extend: {
+            colors: {
+              brand: "#007291",
+            },
+          },
+        },
+      }}
+    >
+      <Html>
+        <Head />
+        <Body>
+          
+          <Button
+            href="https://example.com"
+            className="bg-brand px-3 py-2 font-medium leading-4 text-white"
+          >
+            Click me
+          </Button>
+        </Body>
+      </Html>
+    </Tailwind>
+  );
+};
+
+export default Email;
